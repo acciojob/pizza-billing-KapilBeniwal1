accee23c0dc6bd129b4c7b0e16bcad287be60e99
KAPIL  BENIWAL <kapilbeniwal2813@gmail.com>
2023-01-17 19:21:28 +0530

    Assignment solved(second commit)

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,11 @@
 package com.driver;
 
 public class DeluxePizza extends Pizza {
+    int price;
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes here
+        addExtraCheese();
+        addExtraToppings();
     }
 }
